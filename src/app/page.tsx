db96a0a111228d44f2da201b9668edb779cae462
JAHID HASAN <developerjahid05@gmail.com>
2026-04-11 10:47:23 +0600

    refector: landing page footer and navbar refector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-
+import NavbarSecondary from '@/containers/LandingPage/NavbarSecondary';
+import Footer from '@/components/shared/Footer/Footer';
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import Landing from "./landing/page";
-
+const navItems = [
+  { name: "Home", href: "/landing" },
+  { name: "Challenges", href: "/challenges" },
+];
 export default async function Home() {
   const cookieStore = await cookies();
   const token = cookieStore.get("accessToken")?.value;
@@ -14,7 +18,9 @@ export default async function Home() {
 
   return (
     <div className="scroll-smooth bg-black bg-[url('/images/landing-page-bg.png')] bg-cover font-magseva h-full w-full">
-      <Landing/>
+      <NavbarSecondary navItems={navItems} />
+      <Landing />
+      <Footer />
       {/* <LandingPageScreen /> */}
     </div>
   );
